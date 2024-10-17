1413deb8a5fa05473a6503af0725208752b6f469
guti10501 <afgutierrez4@gmail.com>
2024-10-17 10:47:20 +0900

    4.19: Blog List Expansion, step 7

--- utils/middleware.js
+++ utils/middleware.js
@@ -15,6 +15,8 @@ const errorHandler = (error, request, response, next) => {
     return response.status(400).json({ error: 'expected `username` to be unique' })
   } else if (error.name === 'Error' && error.message.includes('data and salt arguments required')) {
     return response.status(400).json({ error: 'Path `password` is required' })
+  } else if (error.name ===  'JsonWebTokenError') {
+    return response.status(401).json({ error: 'token invalid' })
   }
 
   next(error)
